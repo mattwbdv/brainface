32f258712da6fd46a686c192223130d1b04061f2
Matt Koenig <koenigmattd@gmail.com>
2019-12-25 15:55:06 -0800

    first commit

---
+++ build/precache-manifest.0f2e258fcfa023d9958e4e24617ed0de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6694d14ebf0b82b015b1f5e5bffb6ef6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e4e14d64725466fc3b6c",
+    "url": "/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "87a041447f8c5d97b0c5",
+    "url": "/static/css/main.01431899.chunk.css"
+  },
+  {
+    "revision": "e4e14d64725466fc3b6c",
+    "url": "/static/js/2.703afcb2.chunk.js"
+  },
+  {
+    "revision": "87a041447f8c5d97b0c5",
+    "url": "/static/js/main.a0f74ee3.chunk.js"
+  },
+  {
+    "revision": "e4eea06c216eed3653c1",
+    "url": "/static/js/runtime~main.ff05d067.js"
+  }
+]);
